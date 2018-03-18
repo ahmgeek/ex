55cdbfce9024a48b66df2d5a38f76e458dae0821
Ahmed Tolba <tolpa1@gmail.com>
2018-03-18 22:06:14 +0100

    Add scrapper testing

---
+++ spec/scrapper_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.describe Scrapper do
+  subject { described_class }
+
+  context ".run" do
+    let(:sheet) do
+      OpenStruct.new(date: "2018-03-16", rates: { "USD"=>1.2301,
+                                                  "JPY"=>130.21,
+                                                  "BGN"=>1.9558 })
+    end
+
+    let(:xml) { File.read("spec/fixtures/data.xml").to_s }
+
+    before do
+      expect(subject).to receive(:fetch_currencies)
+        .with(from: 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+        .and_return(xml)
+      expect(subject).to receive(:parse).with(xml).and_return(sheet)
+      expect(subject).to receive(:store_currencies).with(sheet).and_return("OK")
+    end
+
+    it "store data" do
+      expect(subject.run).to eq "OK"
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@
 $LOAD_PATH.unshift File.expand_path('lib', __dir__)
 
 require 'pry'
+require 'scrapper'
+require 'exchange_rate'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |e|
@@ -15,8 +17,4 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  def fixture_file(file_name)
-    File.expand_path("../fixtures/#{file_name}", __FILE__)
-  end
 end
